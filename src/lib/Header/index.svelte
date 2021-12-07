092e0932a128068e485a4dbbe41221537a2cda0c
Mike Brandon <55140732+MikeBrandon@users.noreply.github.com>
2021-12-07 22:44:20 +0300

    add: header text and account summary btn

--- src/lib/Header/index.svelte
+++ src/lib/Header/index.svelte
@@ -1,12 +1,15 @@
-<h2>Maximize yield by staking FIRO</h2>
+<h2>Earn FIRO by Staking your LP Tokens</h2>
 <p class="description">
-	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque efficitur risus metus,
-	suscipit consequat est tempus semper. Suspendisse et urna vel metus pulvinar sagittis. Nunc
-	tristique lobortis scelerisque. Fusce quis gravida ex. In sodales volutpat porttitor. Phasellus
-	tempus eros id leo gravida, in viverra sem viverra. Donec vulputate massa at enim tincidunt
-	hendre. In sodales volutpat porttitor. Phasellus tempus eros id leo gravida, in viverra sem
-	viverra. Donec vulputate massa at enim tincidunt hendre
+	You can stake your FIRO/WBNB Pancakeswap liquidity provision tokens here to earn additional BEP20
+	Firo.
+	<br />
+	<br />
+	***Unstaking your LP, has a cool down of 2 days to encourage users to provide long term liquidity to
+	FIRO on BSC.
 </p>
+<div class="btn-holder">
+	<button> View Account Summary </button>
+</div>
 
 <style lang="postcss">
 	h2 {
@@ -14,6 +17,15 @@
 	}
 
 	.description {
-		@apply text-para-color mt-2 mb-4 text-sm md:text-base xxl:text-xl xxxl:text-4xl xxl:mt-5 xxxl:mt-10 xxxl:mb-28 xxxl:leading-normal;
+		@apply text-para-color mt-2 mb-4 text-sm md:text-base xxl:text-xl xxxl:text-4xl xxl:mt-5 xxxl:mt-10 xxxl:mb-28 xxxl:leading-normal md:mb-8 xl:mb-[52px];
+	}
+
+	button {
+		@apply border border-maincolor md:mb-8 xl:mb-[52px] border-opacity-0  rounded-[120px] py-[14px] px-[60px] xxxl:py-[28px] xxxl:px-[120px] bg-maincolor text-white hover:bg-white hover:border-opacity-100 hover:text-maincolor duration-500;
+		@apply text-sm md:text-base xxl:text-xl xxxl:text-4xl;
+	}
+
+	.btn-holder {
+		@apply w-full flex justify-center items-center;
 	}
 </style>
