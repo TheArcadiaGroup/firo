6b2924ac497b91c49a76dbc1773cb51e63b7f96b
Mike Brandon <55140732+MikeBrandon@users.noreply.github.com>
2021-12-15 23:37:51 +0300

    cleanup

--- src/lib/Stake/index.svelte
+++ src/lib/Stake/index.svelte
@@ -1,28 +1,9 @@
 <script>
 	import Switch from '$lib/Switch/index.svelte';
-	import { onMount } from 'svelte';
 	import { isStaking, walletConnected } from './stakingStore';
+	import TextInput from '$lib/TextInput/index.svelte';
 
-	var input;
-	let inputValue;
 	isStaking.set(true);
-	onMount(() => {
-		input = document.getElementById('text-input');
-	});
-
-	export let balance = 200;
-
-	function isInputNumber(event) {
-		var ch = String.fromCharCode(event.which);
-
-		if (!/[0-9]/.test(ch)) {
-			event.preventDefault();
-		}
-	}
-
-	function inputMax() {
-		input.value = balance;
-	}
 </script>
 
 <div class="main">
@@ -33,21 +14,7 @@
 		<p class="title">Unstake LP TOKEN</p>
 	{/if}
 
-	<div class="text-div">
-		<input
-			id="text-input"
-			bind:value={inputValue}
-			type="text"
-			on:keypress={isInputNumber}
-			placeholder="0 LP TOKEN"
-		/>
-		<div class="max-balance">
-			<p class="balance">Balance: {balance}</p>
-			<div class="button" on:click={inputMax}>
-				<p>MAX</p>
-			</div>
-		</div>
-	</div>
+	<TextInput />
 
 	<div class="lower-text">
 		{#if $isStaking}
@@ -79,31 +46,6 @@
 
 	.title {
 		@apply font-semibold text-xl md:text-2xl xxl:text-4xl xxxl:text-5xl my-4 xxl:my-6 xxxl:my-8;
-	}
-
-	input[type='text'] {
-		@apply md:h-8 w-full mr-3 text-base xxl:text-2xl xxxl:text-4xl xxxl:h-16 outline-none;
-	}
-
-	.text-div {
-		@apply py-2 px-3 md:py-4 md:px-5 bg-white rounded-full border border-line-color flex xxxl:py-6 xxl:border-2 xxxl:border-4;
-		box-shadow: 0px 4px 20px rgba(170, 123, 123, 0.1);
-	}
-
-	.balance {
-		@apply text-black-lighter mr-3 text-sm md:text-base xxl:text-xl xxxl:text-3xl;
-	}
-
-	.max-balance {
-		@apply min-w-max flex items-center;
-	}
-
-	.button > p {
-		@apply text-white text-xs md:text-base xxl:text-xl xxxl:text-3xl;
-	}
-
-	.button {
-		@apply bg-maincolor md:h-full py-1 px-2 md:px-3 xxxl:px-6 rounded-2xl cursor-pointer flex items-center justify-center xxxl:rounded-full;
 	}
 
 	.red-text {
