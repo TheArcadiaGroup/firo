ccba41d3aa4adc83a32c0d965a8cfdf46e01c81e
John Gicharu <89821717+gicharujohn@users.noreply.github.com>
2022-03-30 14:00:16 +0300

    feat: updated contract addresses

--- src/constants/contracts/contractAddresses.ts
+++ src/constants/contracts/contractAddresses.ts
@@ -40,8 +40,8 @@ export const vestingContract = (chainID: number | string) => {
 		: chainID === 97 || parseInt(chainID.toString()) === 97
 		? '0x462Ef7a62846f068962919C0D3F2eC292522de30'
 		: chainID === 56 || parseInt(chainID.toString()) === 56
-		? '0x8b241E1C285b734A4441B57CB5b37E176A65d268'
-		: '0x8b241E1C285b734A4441B57CB5b37E176A65d268';
+		? '0xc4E9634F5CE62e8076DD0EC9e0100efa71Dfd483'
+		: '0xc4E9634F5CE62e8076DD0EC9e0100efa71Dfd483';
 };
 
 export const lockingContract = (chainID: number | string) => {
@@ -50,8 +50,8 @@ export const lockingContract = (chainID: number | string) => {
 		: chainID === 97 || parseInt(chainID.toString()) === 97
 		? '0x98b03026E4CbF6d9Ec17C7022B98521632A531fa'
 		: chainID === 56 || parseInt(chainID.toString()) === 56
-		? '0xF0cc85c592B8652F8fCf43f8Bf02bB5A78038BE1'
-		: '0xF0cc85c592B8652F8fCf43f8Bf02bB5A78038BE1';
+		? '0x6658bAF8a57069f58eCA63ff9A8626aeF5433092'
+		: '0x6658bAF8a57069f58eCA63ff9A8626aeF5433092';
 };
 
 export const masterChef = (chainID: number | string) => {
@@ -60,6 +60,6 @@ export const masterChef = (chainID: number | string) => {
 		: chainID === 97 || parseInt(chainID.toString()) === 97
 		? '0x74FAF62b6b33b19FC433E1e8b325013D267FDAD2'
 		: chainID === 56 || parseInt(chainID.toString()) === 56
-		? '0xDf27b5800A74F54f120939b8a67A45b268A5fC20'
-		: '0xDf27b5800A74F54f120939b8a67A45b268A5fC20';
+		? '0x1175F3E59C8DB26d9Cdf0aE8eb15b40E6a236c6A'
+		: '0x1175F3E59C8DB26d9Cdf0aE8eb15b40E6a236c6A';
 };
